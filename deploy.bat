4f258e980a5c5db56ebf044a5558e789f808c19f
unknown <1152283137@qq.com>
2026-01-31 16:18:56 +0800

    Fix chart JSON block parsing

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,13 @@ cd /d %~dp0
 
 git add .
 git commit -m "daily update %date% %time%"
-git push
 
-echo Done!
+git pull --rebase origin main
+IF ERRORLEVEL 1 (
+  echo [ERROR] pull failed. fix conflicts then run again.
+  pause
+  exit /b 1
+)
+
+git push origin main
 pause
